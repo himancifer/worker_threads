e7b9544600cf6570550dbad05e28c73320acf260
himancifer <himangshushekharjha@gmail.com>
2020-08-28 16:27:42 +0530

    Done with fillArray

--- fillArray.js
+++ fillArray.js
@@ -1,33 +1,55 @@
-const { Worker, isMainThread, parentPort , workerData } = require('worker_threads');
+const { Worker, isMainThread, parentPort, MessageChannel , workerData } = require('worker_threads');
 
-const workers = [];
+const { port1 : mainPort, port2 : workerPort} = new MessageChannel();
+const os = require('os'); 
 
 if(isMainThread){
-    console.log('Main thread');
     const startTime = Date.now();
-    const size = 1_000_000_000;
-    // const size = 100000000;
-    const sharedBuffer = new SharedArrayBuffer(Int32Array.BYTES_PER_ELEMENT*size);
+    console.log(os.cpus().length);
+    const workers = [];
+    console.log('Main thread');
+    
+    const numberOfElements = 1_000_000_000;
+    const numberOfThreads =  4; //os.cpus().length;
+    
+    const sharedBuffer = new SharedArrayBuffer(Int32Array.BYTES_PER_ELEMENT*numberOfElements);
     const arr = new Int32Array(sharedBuffer);
 
-    for(let i = 0;  i < size ; i++){
-        arr[i] = i + 2;
-    }
-    const endTime = Date.now();
-    console.log((endTime - startTime)/1000, 'seconds to complete');    
+    let completed = 0;
+    
+    const numberOfElementsPerThread = Math.ceil(numberOfElements/numberOfThreads);
 
-    console.log(arr);
-
-    while(workers.length < 4){
-        const worker = new Worker(__filename);
+    while(workers.length < numberOfThreads){
+        const start = workers.length * numberOfElementsPerThread;
+        const end  = start + numberOfElementsPerThread;
+        const worker = new Worker(__filename, {
+            workerData : {
+                index : workers.length,
+                arr,
+                start,
+                end
+            }
+        });
+        worker.on('message',(message) => {
+            if(message.completed)
+                completed++;
+            if(completed === numberOfThreads){
+                console.log("Totally Done");
+                console.log(arr);
+                const endTime = Date.now();
+                console.log((endTime - startTime)/1000,'seconds to complete');
+            }
+        });
         workers.push(worker);
     }
 }
 
 else{
-    console.log("worker thread");
-    
+    console.log(workerData.index,workerData.start,workerData.end);
+    for(let i = workerData.start;  i < workerData.end ; i++){
+        workerData.arr[i] = i + 2;
+    }
+
+    parentPort.postMessage({ completed : true }); 
 }
 
-
-console.log('No of worker threads as of now ----> ',workers.length);
